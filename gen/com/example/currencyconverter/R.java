f18be1fa5f526dfc5c2d249f76747ae5fd090a66
Erik Bartlett <eabartlett@berkeley.edu>
2014-01-31 11:20:45 -0800

    Basic converter functionality implimented, only need to fix interface and possibly add refreshing/ability to add currencies

--- gen/com/example/currencyconverter/R.java
+++ gen/com/example/currencyconverter/R.java
@@ -27,9 +27,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090003;
+        public static final int action_settings=0x7f090004;
         public static final int convert_from=0x7f090000;
         public static final int convert_to=0x7f090001;
+        public static final int converted_value=0x7f090003;
         public static final int value_to_convert=0x7f090002;
     }
     public static final class layout {
--- src/com/example/currencyconverter/ConverterMain.java
+++ src/com/example/currencyconverter/ConverterMain.java
@@ -2,10 +2,13 @@ package com.example.currencyconverter;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -22,6 +25,31 @@ public class ConverterMain extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_converter_main);
+		
+		EditText editText = (EditText) findViewById(R.id.value_to_convert);
+		editText.addTextChangedListener(new TextWatcher(){
+			@Override
+			public void afterTextChanged(Editable s){
+				setAmount();
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// Do nothing
+				return;
+				
+			}
+
+			@Override
+			public void onTextChanged(CharSequence arg0, int arg1, int arg2,
+					int arg3) {
+				// Do nothing
+				return;
+				
+			}
+		});
+		
 		Spinner convertFromSpinner = (Spinner) findViewById(R.id.convert_from);
 		Spinner convertToSpinner = (Spinner) findViewById(R.id.convert_to);
 		//Create Adapter Array from string array
@@ -32,6 +60,54 @@ public class ConverterMain extends Activity {
 		//Apply adapter to spinners
 		convertFromSpinner.setAdapter(adapter);
 		convertToSpinner.setAdapter(adapter);
+
+		//set spinner's listener
+		convertFromSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+				switch (pos) {
+					case 0:
+						convertFrom = DC;
+						break;
+					case 1:
+						convertFrom = BC;
+						break;
+					case 2:
+						convertFrom = USD;
+						break;
+					default:
+						break;
+				}
+				setAmount();
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent){
+				return;
+			}
+		});
+		convertToSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
+				switch (pos) {
+					case 0:
+						convertTo = DC;
+						break;
+					case 1:
+						convertTo = BC;
+						break;
+					case 2:
+						convertTo = USD;
+						break;
+					default:
+						break;
+				}
+				setAmount();
+			}
+			@Override
+			public void onNothingSelected(AdapterView<?> parent){
+				return;
+			}
+		});
 	}
 
 	@Override
@@ -41,28 +117,27 @@ public class ConverterMain extends Activity {
 		return true;
 	}
 	
-	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id){
-		System.out.println("Erik");
-		if (parent.equals(R.id.convert_from)){
-			switch (pos) {
-			case 0:
-				convertFrom = USD;
-			case 1:
-				convertFrom = BC;
-			case 2:
-				convertFrom = DC;
-			}
-		}else {
-			switch (pos) {
-			case 0:
-				convertTo = USD;
-			case 1:
-				convertTo = BC;
-			case 2:
-				convertTo = DC;
-			}
-		}
-		TextView t = (TextView) findViewById(R.id.convert_from);
-		t.setText((int) convertFrom);
+	private double convertCurrency(double amount){
+		// ratio of two currencies
+		double conversionRate = convertFrom / convertTo;		
+		// Divide by the worth of currency being converted to
+		return conversionRate * amount;
 	}
+
+	private void setAmount() {
+		EditText editText = (EditText) findViewById(R.id.value_to_convert);
+		String stringAmount = editText.getText().toString();
+		double amount;
+		try {
+			amount = Double.valueOf(stringAmount);
+		} catch (NumberFormatException e) {
+			amount = 0.0;
+		}
+		
+		double convertedAmount = convertCurrency(amount);
+		
+		TextView text = (TextView) findViewById(R.id.converted_value);
+		text.setText(String.valueOf(convertedAmount));
+	}
+	
 }
